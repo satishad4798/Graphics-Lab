6e29e4a9fb93d1ab96064ad454036c286267c1e3
Satish Ad47 <48507510+satishad4798@users.noreply.github.com>
2020-02-18 19:01:16 +0530

    Update cubePyramid.c

--- cubePyramid.c
+++ cubePyramid.c
@@ -1,32 +1,31 @@
 
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
  
-/* Global variables */
+
 char title[] = "3D Shapes";
  
-/* Initialize OpenGL Graphics */
+
 void initGL() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClearDepth(1.0f);                   // Set background depth to farthest
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+   glClearDepth(1.0f);                   
    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
    glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
    glShadeModel(GL_SMOOTH);   // Enable smooth shading
    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
  
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
+
 void display() {
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
  
-   // Render a color-cube consisting of 6 quads with different colors
+   
    glLoadIdentity();                 // Reset the model-view matrix
    glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
  
    glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
       // Top face (y = 1.0f)
-      // Define vertices in counter-clockwise (CCW) order with normal pointing out
+    
       glColor3f(0.0f, 1.0f, 0.0f);     // Green
       glVertex3f( 1.0f, 1.0f, -1.0f);
       glVertex3f(-1.0f, 1.0f, -1.0f);
@@ -110,10 +109,9 @@ void display() {
    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
  
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-   // Compute aspect ratio of the new window
+
+void reshape(GLsizei width, GLsizei height) {  
+  
    if (height == 0) height = 1;                // To prevent divide by 0
    GLfloat aspect = (GLfloat)width / (GLfloat)height;
  
@@ -127,16 +125,16 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 }
  
-/* Main function: GLUT runs as a console application starting at main() */
+
 int main(int argc, char** argv) {
-   glutInit(&argc, argv);            // Initialize GLUT
-   glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-   glutInitWindowSize(640, 480);   // Set the window's initial width & height
-   glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-   glutCreateWindow(title);          // Create window with the given title
-   glutDisplayFunc(display);       // Register callback handler for window re-paint event
-   glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-   initGL();                       // Our own OpenGL initialization
-   glutMainLoop();                 // Enter the infinite event-processing loop
+   glutInit(&argc, argv);           
+   glutInitDisplayMode(GLUT_DOUBLE);
+   glutInitWindowSize(640, 480); 
+   glutInitWindowPosition(50, 50); 
+   glutCreateWindow(title);        
+   glutDisplayFunc(display);     
+   glutReshapeFunc(reshape);      
+   initGL();                     
+   glutMainLoop();               
    return 0;
 }
